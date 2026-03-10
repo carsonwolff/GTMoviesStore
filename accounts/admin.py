2a8de89944eaf900480742d545bbc10075ba2235
Suri, Arsh <suriarsh@gmail.com>
2026-03-10 02:17:18 -0400

    Adding Feature for MostFrequentBuyer
    Added extension of User called Profile, which has the movie_count field, which represents the movies bought. Added logic to handle previous users.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.db.models import Count, Sum
+from django.template.response import TemplateResponse
+from django.urls import path
+from cart.models import Item
+from .models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+class ProfileAdmin(UserAdmin):
+    inlines = [ProfileInLine]
+    list_display = UserAdmin.list_display + ('get_movie_count', )
+    def get_movie_count(self, object):
+        try:
+            return object.profile.movie_count
+        except Profile.DoesNotExist:
+            return 0
+    get_movie_count.short_description = 'Movies bought by User'
+
+admin.site.unregister(User)
+admin.site.register(User, ProfileAdmin)
+
